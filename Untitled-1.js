e109a4b0688db746970e553be359276b3303bac6
ashwini99 <ashwini9999>
2020-01-19 12:09:54 +0530

    commit

--- Untitled-1.js
+++ Untitled-1.js
@@ -1,22 +1,5 @@
-var dialogNames = ["Main", "Wanderer", "Spyros", "Equilibris", "NevaArte", "Capella"];
-if ("IntersectionObserver" in window) {
-	var firstGap = document.getElementsByClassName("gap")[0],
-		isSent = !1,
-		options = {
-			rootMargin: "0px",
-			threshold: .5
-		},
-		sendEvent = function (e, n) {
-			e.forEach(function (e) {
-				e.intersectionRatio > 0 && !isSent && (gtag("event", "Scroll", {
-					event_category: "Page",
-					event_label: "Second block scrolled"
-				}), isSent = !0)
-			})
-		},
-		observer = new IntersectionObserver(sendEvent, options);
-	observer.observe(firstGap)
-}
+
+
 const disableBodyScroll = bodyScrollLock.disableBodyScroll,
 	enableBodyScroll = bodyScrollLock.enableBodyScroll;
 var config = {
@@ -87,22 +70,6 @@ function animate() {
 function drawFrame() {
 	let e = config.boards[config.scrollNow];
 	e.current > 0 ? e.current = .9 * e.current + config.delta / 4 : e.current < e.w ? e.current = e.w - .9 * (e.w - e.current) + config.delta / 4 : e.current = e.prev + config.delta, config.delta *= .9, e.el.style = "transform: translate3D(" + e.current + "px, 0, 0 );", e.prev = e.current
-}
-
-function openDialog(e) {
-	d.num = e, d.board = config.boards[e], d.dialog = config.boards[e].el.parentNode, d.board.prev = 0, d.board.current = 0, d.board.el.style = "", d.dialog.scroll(0, 0), d.dialog.classList.add("dialog-visible"), d.dialog.classList.remove("dialog-hidden"), d.main.classList.add("main-hidden"), d.main.classList.remove("main-visible"), config.isLandscape || (disableBodyScroll(d.dialog), d.isBgBlocked = !0, d.isLandscape = config.isLandscape), config.animStart = performance.now(), config.scrollNow = e, gtag("event", "Open", {
-		event_category: "Dialog",
-		event_label: dialogNames[e]
-	})
-}
-
-function closeDialog(e) {
-	d.dialog.classList.remove("dialog-visible"), d.dialog.classList.add("dialog-hidden"), d.main.classList.remove("main-hidden"), d.main.classList.add("main-visible"), config.isLandscape || (enableBodyScroll(d.dialog), d.isBgBlocked = !1), config.animStart = performance.now();
-	var n = dialogNames[config.scrollNow] + "-" + e;
-	gtag("event", "Close", {
-		event_category: "Dialog",
-		event_label: n
-	}), config.scrollNow = 0
 }
 
 function setVh() {
